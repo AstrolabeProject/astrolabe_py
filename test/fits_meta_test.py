9632f9af0871b028b65f6d4253479caec4e802ea
Tom Hicks <hickst@tohono.com>
2018-06-25 20:52:58 -0700

    Add tests of handle_ctype_mapping.

--- test/fits_meta_test.py
+++ test/fits_meta_test.py
@@ -2,8 +2,9 @@
 #
 # Python code to (unit) test the Astrolabe FITS Metadata module.
 #   Written by: Tom Hicks. 6/22/2018.
-#   Last Modified: Initial creation.
+#   Last Modified: Add tests of handle_ctype_mapping.
 #
+from astropy.io import fits
 import astrolabe_uploader.fits_meta as fm
 import unittest
 
@@ -19,6 +20,7 @@ class FitsMetaTestCase(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
     cls.default_options = { "keyfile": "metadata-keys.txt" }
+    cls.test_files = [ "cvnidwabcut.fits" ]
 
 
 class GetMetadataKeysTestCase(FitsMetaTestCase):
@@ -46,6 +48,43 @@ class GetMetadataKeysTestCase(FitsMetaTestCase):
     self.assertIn("filepath", keys)
 
 
+class HandleCtypeMappingTestCase(FitsMetaTestCase):
+
+  def setUp(self):
+    "Initialize the test case"
+    datafile = fits.open(self.test_files[0])
+    self.file_metadata = datafile[0].header
+    datafile.close()
+    print("TYPE=" + str(type(self.file_metadata)))
+
+  def test_nocrval(self):
+    metadata = []
+    self.assertFalse(metadata)
+    fm.handle_ctype_mapping('', self.file_metadata, metadata)
+    fm.handle_ctype_mapping('BADKEY', self.file_metadata, metadata)
+    self.assertFalse(metadata)
+
+  def test_crval1(self):
+    metadata = []
+    self.assertFalse(metadata)
+    fm.handle_ctype_mapping('CRVAL1', self.file_metadata, metadata)
+    self.assertTrue(metadata)
+    self.assertEqual(len(metadata), 2)
+    self.assertTrue(all([isinstance(md, tuple) for md in metadata]))
+    self.assertNotIn("CRVAL2", [md[0] for md in metadata])
+    self.assertIn("right_ascension", [md[0] for md in metadata])
+
+  def test_crval2(self):
+    metadata = []
+    self.assertFalse(metadata)
+    fm.handle_ctype_mapping('CRVAL2', self.file_metadata, metadata)
+    self.assertTrue(metadata)
+    self.assertEqual(len(metadata), 2)
+    self.assertTrue(all([isinstance(md, tuple) for md in metadata]))
+    self.assertNotIn("CRVAL1", [md[0] for md in metadata])
+    self.assertIn("declination", [md[0] for md in metadata])
+
+
 if __name__ == '__main__':
   suite = suite()
-  unittest.TextTestRunner(verbosity = 2).run(suite)
+  unittest.TextTestRunner(verbosity=2).run(suite)
