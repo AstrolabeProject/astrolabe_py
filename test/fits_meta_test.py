e5d2c7d9a9a52080e6e360d732fe62491346e536
Tom Hicks <hickst@tohono.com>
2018-06-26 09:09:59 -0700

    Get Suite working correctly. Add tests of fits_metadata.

--- test/fits_meta_test.py
+++ test/fits_meta_test.py
@@ -1,8 +1,8 @@
-# #!/usr/bin/env python3
+#!/usr/bin/env python3
 #
 # Python code to (unit) test the Astrolabe FITS Metadata module.
 #   Written by: Tom Hicks. 6/22/2018.
-#   Last Modified: Add tests of handle_ctype_mapping.
+#   Last Modified: Get Suite working correctly. Add tests of fits_metadata.
 #
 from astropy.io import fits
 import astrolabe_uploader.fits_meta as fm
@@ -10,7 +10,9 @@ import unittest
 
 def suite():
   suite = unittest.TestSuite()
-  suite.addTest(unittest.TestLoader().loadTestsFromTestCase(GetMetadataKeysTestCase))
+  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(GetMetadataKeysTestCase))
+  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(HandleCtypeMappingTestCase))
+  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(FitsMetadataTestCase))
   return suite
 
 
@@ -55,7 +57,6 @@ class HandleCtypeMappingTestCase(FitsMetaTestCase):
     datafile = fits.open(self.test_files[0])
     self.file_metadata = datafile[0].header
     datafile.close()
-    print("TYPE=" + str(type(self.file_metadata)))
 
   def test_nocrval(self):
     metadata = []
@@ -83,6 +84,39 @@ class HandleCtypeMappingTestCase(FitsMetaTestCase):
     self.assertTrue(all([isinstance(md, tuple) for md in metadata]))
     self.assertNotIn("CRVAL1", [md[0] for md in metadata])
     self.assertIn("declination", [md[0] for md in metadata])
+
+
+class FitsMetadataTestCase(FitsMetaTestCase):
+
+  def setUp(self):
+    "Initialize the test case"
+    pass
+
+  def test_bad_filepath(self):
+    "Throws exception on bad file path"
+    self.assertRaises(FileNotFoundError,
+                      fm.fits_metadata, "NO_SUCH_FILEPATH", self.default_options)
+
+  def test_no_metadata_keys(self):
+    "Extract no metadata if no metadata keys specified"
+    mdata = fm.fits_metadata(self.test_files[0], {"keyfile": "empty-metadata-keyfile.txt"})
+    self.assertNotEqual(mdata, None)
+    self.assertEqual(len(mdata), 0)
+    self.assertNotIn("filepath", mdata)
+
+  def test_metadata(self):
+    "Extract metadata if default metadata keys specified"
+    metadata = fm.fits_metadata(self.test_files[0], self.default_options)
+    # print(metadata)                         # DEBUGGING
+    self.assertNotEqual(metadata, None)
+    self.assertEqual(len(metadata), 17)
+
+    mdkeys = [md[0] for md in metadata]
+    self.assertIn("filepath", mdkeys)
+    self.assertIn("right_ascension", mdkeys)
+    self.assertIn("declination", mdkeys)
+    self.assertIn("obs_title", mdkeys)
+    self.assertIn("ORIGIN", mdkeys)
 
 
 if __name__ == '__main__':
