c5546c8565d25dd1b0f57b2dc14b216bb536524f
Tom Hicks <hickst@tohono.com>
2018-07-05 21:18:09 -0700

    Add some connection tests.

--- test/irods_help_test.py
+++ test/irods_help_test.py
@@ -2,10 +2,10 @@
 #
 # Python code to unit test the Astrolabe iRods Help class.
 #   Written by: Tom Hicks. 6/30/2018.
-#   Last Modified: Initial creation.
+#   Last Modified: Add some connection tests.
 #
 import unittest
-#from irods.session import iRODSSession
+from irods.session import iRODSSession
 
 from context import ih                      # the module under test
 
@@ -17,7 +17,7 @@ def suite():
 
 class IrodsHelpTestCase(unittest.TestCase):
 
-  "Base test class"
+  """ Base test class """
   @classmethod
   def setUpClass(cls):
     cls.default_options = { "irods_env_file": "irods_env_file.json" }
@@ -27,16 +27,40 @@ class IrodsHelpTestCase(unittest.TestCase):
 class ConnectionsTestCase(IrodsHelpTestCase):
 
   def setUp(self):
-    "Initialize the test case"
-    pass
+    """ Initialize the test case """
+    self.helper = ih.IrodsHelper()            # create instance of class under test
 
   def tearDown(self):
-    "Cleanup the test case"
-    pass
+    """ Cleanup the test case """
+    self.helper.disconnect()
 
-  def test_one(self):
-    "Do some testing"
-    self.assertEqual(self.irods_env_file, "irods_env_file.json") # REPLACE LATER
+  def test_helper(self):
+    """ Test that the helper instance has been created. """
+    self.assertNotEqual(self.helper, None)
+
+  def test_connected(self):
+    """ Test that the helper is connected (a session has been created). """
+    self.helper.connect({})                 # no options
+    self.assertTrue(self.helper.is_connected())
+
+  def test_disconnect(self):
+    """ Test that the connection has been closed """
+    self.helper.disconnect()
+    self.assertFalse(self.helper.is_connected())
+
+  def test_get_session(self):
+    """ Test getting the session from the helper. """
+    self.helper.connect({})                 # no options
+    sess = self.helper.session()
+    self.assertNotEqual(sess, None)
+    self.assertEqual(type(sess), iRODSSession)
+
+  def test_get_cwd(self):
+    """ Test getting the current directory from the helper. """
+    self.helper.connect({})                 # no options
+    cwd = self.helper.cwd()
+    self.assertNotEqual(cwd, None)
+    self.assertEqual(type(cwd), str)
 
 
 if __name__ == '__main__':
