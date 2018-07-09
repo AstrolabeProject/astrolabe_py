9b7b57ce83cbf914c149671f8a0b6216f37ce865
Tom Hicks <hickst@tohono.com>
2018-07-09 13:49:32 -0700

    Redo handle for ctype mapping. Consistent quotes.

--- astrolabe_uploader/fits_meta.py
+++ astrolabe_uploader/fits_meta.py
@@ -1,24 +1,29 @@
 #
 # Module to view, extract, and/or verify metadata from one or more FITS files.
 #   Written by: Tom Hicks. 4/24/2018.
-#   Last Modified: Port to Uploader.
+#   Last Modified: Redo handle for ctype mapping. Consistent quotes.
 #
 import warnings
 import sys
 from astropy.io import fits
 
-# Dictionary of alternates for standard FITS metadata keys
+# dictionary of alternates for standard FITS metadata keys
 ALTERNATE_KEYS_MAP = {
-'spatial_axis_1_number_bins': 'NAXIS1',
-'spatial_axis_2_number_bins': 'NAXIS2',
-'start_time': 'DATE-OBS',
-'facility_name': 'INSTRUME',
-'instrument_name': 'TELESCOP',
-'obs_creator_name': 'OBSERVER',
-'obs_title': 'OBJECT'
+"spatial_axis_1_number_bins": "NAXIS1",
+"spatial_axis_2_number_bins": "NAXIS2",
+"start_time": "DATE-OBS",
+"facility_name": "INSTRUME",
+"instrument_name": "TELESCOP",
+"obs_creator_name": "OBSERVER",
+"obs_title": "OBJECT"
 }
 ALTERNATE_KEYS = set(ALTERNATE_KEYS_MAP.keys())
-FILEPATH_KEY = 'filepath'
+
+# dictionary mapping CRVALs to their interpretation items
+CRVALS = { "CRVAL1": "CTYPE1", "CRVAL2": "CTYPE2" }
+CRVAL_KEYS = set(CRVALS.keys())
+
+FILEPATH_KEY = "filepath"
 
 def extract_metadata(file_path, hdu, desired_keys):
     """Extract the metadata from the HeaderDataUnit of the given file for the keys
@@ -32,12 +37,12 @@ def extract_metadata(file_path, hdu, desired_keys):
             elif (key in ALTERNATE_KEYS):                   # is this an alternate key?
                 standard_key = ALTERNATE_KEYS_MAP[key]      # get more standard key
                 metadata.append( (key, file_metadata.get(standard_key)) )
-            elif ((key == 'CRVAL1') or (key == 'CRVAL2')):
+            elif (key in CRVAL_KEYS):
                 handle_ctype_mapping(key, file_metadata, metadata)
             else:                                           # just lookup the given key
                 metadata.append( (key, file_metadata.get(key)) )
         except KeyError:
-            metadata.append( (key, '') )
+            metadata.append( (key, "") )
     return metadata
 
 
@@ -55,12 +60,12 @@ def fits_info(file_path, options=None):
     with fits.open(file_path) as hdus:
         print(hdus.info())
         for hdu in hdus:
-            hdu.verify('silentfix+ignore')
+            hdu.verify("silentfix+ignore")
             hdr = hdu.header
             for key in hdr.keys():
                 val = str(hdr[key])
                 if (key and val):                   # ignore blank keys or values
-                    print(key + ': ' + val)
+                    print(key + ": " + val)
         print()
 
 
@@ -71,7 +76,7 @@ def fits_verify(file_path, options=None):
     problems_file = "problems.txt"
     with fits.open(file_path) as hdu:
         with warnings.catch_warnings(record=True) as warns:
-            hdu.verify('fix+warn')
+            hdu.verify("fix+warn")
             if (warns and len(warns) > 0):
                 print("Filename: " + file_path)
                 for warn in warns:
@@ -81,28 +86,22 @@ def fits_verify(file_path, options=None):
 def get_metadata_keys(options):
     "Return a list of metadata keys to be extracted"
     keyfile = options.get("keyfile")
-    with open(keyfile, 'r') as mdkeys_file:
+    with open(keyfile, "r") as mdkeys_file:
         return mdkeys_file.read().splitlines()
 
 
 def handle_ctype_mapping(key, file_metadata, metadata):
-    """If key is a special value which provides the meaning of other fields, then
-       add the key and its value and then write the referenced value with a another key.
-       For CRVALs and how they relate to CRTYPEs see https://fits.gsfc.nasa.gov/fits_standard.html
+    """ If a metadata item has a CRVAL* key, it holds a value whose interpretation is defined
+        by a corresponding CTYPE* metadata item. Add the base CRVAL item and then add another
+        item with an interpretated key and the CRVAL value.
+        For CRVALs and how they relate to CTYPEs see https://fits.gsfc.nasa.gov/fits_standard.html
     """
-    if (key == 'CRVAL1'):
-        metadata.append( ('CRVAL1', file_metadata.get('CRVAL1')) )
-        if 'RA' in file_metadata['CTYPE1']:
-            metadata.append( ('right_ascension', file_metadata.get('CRVAL1')) )
-        elif 'DEC' in file_metadata['CTYPE1']:
-            metadata.append( ('declination', file_metadata.get('CRVAL1')) )
-        else:
-            metadata.append( (key, '') )
-    elif (key == 'CRVAL2'):
-        metadata.append( ('CRVAL2', file_metadata.get('CRVAL2')) )
-        if 'RA' in file_metadata['CTYPE2']:
-            metadata.append( ('right_ascension', file_metadata.get('CRVAL2')) )
-        elif 'DEC' in file_metadata['CTYPE2']:
-            metadata.append( ('declination', file_metadata.get('CRVAL2')) )
-        else:
-            metadata.append( (key, '') )
+    ctype_key = CRVALS.get(key)             # get CTYPE* key interpreting the CRVAL* item
+    if (ctype_key):                         # sanity check: CRVAL* key must be CRVALS dictionary
+        metadata.append( (key, file_metadata.get(key)) )
+        if "RA" in file_metadata[ctype_key]:
+            metadata.append( ("right_ascension", file_metadata.get(key)) )
+        elif "DEC" in file_metadata[ctype_key]:
+            metadata.append( ("declination", file_metadata.get(key)) )
+        else:                               # we only handle these interpretations, so far
+            pass
