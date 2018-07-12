3f0e5b05bd9ab82f8d942b5005ca55ff860d4c8e
Tom Hicks <hickst@tohono.com>
2018-07-12 13:25:40 -0700

    Rename Metadatum tuple. Use accessor methods.

--- astrolabe_uploader/fits_meta.py
+++ astrolabe_uploader/fits_meta.py
@@ -1,6 +1,6 @@
 """
 Class to extract and format metadata from FITS files.
-  Last Modified: Initial creation.
+  Last Modified: Rename Metadatum tuple. Use accessor methods.
 """
 __version__ = "0.0.1"
 __author__ = "Tom Hicks"
@@ -12,29 +12,33 @@ from astropy.io import fits
 
 logging.basicConfig(level=logging.ERROR)    # default logging configuration
 
-Metadata = collections.namedtuple('Metadata', ['keyword', 'value'])
+Metadatum = collections.namedtuple('Metadatum', ['keyword', 'value'])
 
 class FitsMeta:
     """ Class to extract and format metadata from FITS files. """
 
     def __init__(self, filename):
-        self.filename = filename
-        hdulist = fits.open(filename)       # raises error if unable to read file
+        self._filename = filename
+        hdulist = fits.open(self._filename) # raises error if unable to read file
         hdu0 = hdulist[0]                   # get first HDU
-        hdu0.verify('silentfix+ignore')     # fix the first HDU
-        self.metadata = [Metadata(c[0], c[1]) for c in hdu0.header.items()]
+        hdu0.verify('silentfix+ignore')     # fix fixable items in the first HDU
+        self._metadata = [Metadatum(c[0], c[1]) for c in hdu0.header.items() if(c[0] and c[1])]
         hdulist.close()                     # close the file
 
     def __enter__(self):
         return self
 
+    def __iter__(self):
+        for md in self._metadata:
+            yield md
+
     def __len__(self):
-        return len(self.metadata)
+        return len(self._metadata)
 
     def filename(self):
         """ Return the filename of the file used by this class. """
-        return self.filename
+        return self._filename
 
     def metadata(self):
       """ Return the metadata items. """
-      return self.metadata
+      return self._metadata
