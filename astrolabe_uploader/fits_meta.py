6bff1d2dfe19d8accf2d230a63db7adecc3dbd5b
Tom Hicks <hickst@tohono.com>
2018-07-14 15:56:55 -0700

    Revert methods to stateful paradigm. Add copy-item, get, and update_key_set methods.

--- astrolabe_uploader/fits_meta.py
+++ astrolabe_uploader/fits_meta.py
@@ -1,8 +1,8 @@
 """
 Class to extract and format metadata from FITS files.
-  Last Modified: Move as_json and filter_by_* methods to module scope.
+  Last Modified: Revert methods to stateful paradigm. Add copy-item, get, and update_key_set methods.
 """
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "Tom Hicks"
 
 import collections
@@ -17,31 +17,12 @@ logging.basicConfig(level=logging.ERROR)    # default logging configuration
 # class to hold an individual metadatum, a list of which is the metadata
 Metadatum = collections.namedtuple('Metadatum', ['keyword', 'value'])
 
-def as_json(metadata):
-    """ Format and return the given metadata as a JSON string. """
-    return json.dumps(metadata)
-
 def default_cleaner_fn(fld):
     """ Return a copy of the given field cleaned up by removing any unwanted characters. """
     if (isinstance(fld, str)):
         return re.sub("[\"\'\\\\]", "", fld) # remove quotes and backslashes
     else:
         return fld
-
-def filter_by_fn(metadata, predicate=None):
-    """ Return a list of Metadatum items passing the given filter predicate.
-        If filter predicate is not given, the identity function is assumed
-        and all elements of iterable that are false are removed.
-    """
-    return list(filter(predicate, metadata))
-
-def filter_by_keys(metadata, keys):
-    """ Return a list of Metadatum items whose keys are in the given metadata. """
-    return filter_by_fn(metadata, lambda item: item.keyword in set(keys))
-
-def key_set(metadata):
-    """ Return a list of the keys for the given metadata. """
-    return set([item.keyword for item in metadata])
 
 
 class FitsMeta:
@@ -57,7 +38,7 @@ class FitsMeta:
         hdu0.verify('silentfix+ignore')     # fix fixable items in the first HDU
         self._metadata = self._extract_metadata(hdu0.header, cleaner)
         self._metadata.append(Metadatum(self.FILEPATH_KEY, self._filepath))
-        self._key_set = key_set(self._metadata)
+        self._update_key_set()              # compute initial set of metadata keys
         hdulist.close()                     # close the file
 
     def __enter__(self):
@@ -65,6 +46,12 @@ class FitsMeta:
 
     def __contains__(self, keyword):
         return keyword in self._key_set
+
+    def __getitem_(self, keyword):
+        item = self.get(keyword)
+        if (item):
+            return item
+        raise KeyError("Key '{}' not found in this metadata".format(keyword))
 
     def __iter__(self):
         for md in self._metadata:
@@ -74,9 +61,42 @@ class FitsMeta:
         return len(self._metadata)
 
 
+    def copy_item(self, src_key, target_key, nodup=False):
+        """ Copy an existing metadatum, named by the src_key, back into the metadata
+            with a new key specified by target_key. If nodup flag is True, then the copy
+            is prevented if it would create a duplicate of an existing metadata key.
+            If the metadatum is successfully copied, the internal key set is updated.
+            Returns True if metadatum copied, False otherwise.
+        """
+        copied = False
+        src_entry = self.get(src_key)
+        if (src_entry):
+            if ((target_key not in metadata) or (not nodup)): # if no target or duplicates allowed
+                copy = src_entry._replace(keyword=target_key, value=src_entry.value)
+                self._metadata.append(copy)
+                self._update_key_set()
+                copied = True
+        return copied
+
     def filepath(self):
         """ Return the filepath of the file used by this class. """
         return self._filepath
+
+    def filter_by_keys(self, keys):
+        """ Return a list of Metadatum items whose keys are in the metadata. """
+        return list(filter(lambda item: item.keyword in set(keys), self._metadata))
+
+    def get(self, keyword, not_found=None):
+        """ Return the first metadatum with the given key or the not_found value, if
+            an entry with the specified key is not present in the metadata.
+        """
+        if (type(keyword) != str):
+            raise TypeError("The key for metadata items must be a string")
+        if (keyword in self._key_set):
+            for item in self._metadata:
+                if (item.keyword == keyword):
+                    return item
+        return default
 
     def hdu_info(self):
         """ Return summary info for all HDUs in the input file. """
@@ -97,11 +117,11 @@ class FitsMeta:
         ks = self._key_set
         if (keys):
             ks = set(keys)
-        return filter_by_keys(self._metadata, ks)
+        return self.filter_by_keys(ks)
 
     def metadata_as_json(self):
         """ Return the metadata items as JSON. """
-        return as_json(self._metadata)
+        return json.dumps(self._metadata)
 
 
     def _extract_metadata(self, header, cleaner):
@@ -113,3 +133,7 @@ class FitsMeta:
             if (key and val):
                 metadata.append(Metadatum(key, val))
         return metadata
+
+    def _update_key_set(self):
+        """ Recompute and save the list of keys for the current metadata. """
+        self._key_set = set([item.keyword for item in self._metadata])
