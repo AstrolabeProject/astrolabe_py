8cc3e0c0fd8a4d27d4eaf250f3f2229869c97411
Tom Hicks <hickst@tohono.com>
2018-07-25 13:34:52 -0700

    Add uploader import to test context.

--- test/context.py
+++ test/context.py
@@ -1,7 +1,7 @@
 #
 # Test context file: obviate need to install module before testing.
 #   Written by: Tom Hicks. 6/30/2018.
-#   Last Modified: Update for rename of fits ops class and test.
+#   Last Modified: Add uploader import.
 #
 import os
 import sys
@@ -10,4 +10,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import astrolabe_uploader.fits_meta as fm
 import astrolabe_uploader.fits_ops as fo
 import astrolabe_uploader.irods_help as ih
+import astrolabe_uploader.uploader as up
 # import astrolabe_uploader.wwt_help as wh
