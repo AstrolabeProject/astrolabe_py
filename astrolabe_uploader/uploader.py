264bb83be2ed76e70d314a6c9a1dcddb4b75da31
Tom Hicks <hickst@tohono.com>
2018-07-26 19:05:13 -0700

    Refactor filter to utils module. Implement rudimentary do_file.

--- astrolabe_uploader/uploader.py
+++ astrolabe_uploader/uploader.py
@@ -1,7 +1,7 @@
 #
 # Module to extract metadata and upload one or more FITS files to iRods.
 #   Written by: Tom Hicks. 7/19/2018.
-#   Last Modified: Initial working version of do_file.
+#   Last Modified: Refactor filter to utils module. Implement rudimentary do_file.
 #
 import os
 import sys
@@ -10,19 +10,20 @@ import fnmatch
 
 import astrolabe_uploader.fits_ops as fo
 import astrolabe_uploader.irods_help as ih
+import astrolabe_uploader.utils
 
 logging.basicConfig(level=logging.INFO)     # default logging configuration
 
 _ASTROLABE_ROOT_DIR = "astrolabe"
 
-def do_file(action, file_path, options):
-    """ Do the specified action(s) on the given file. """
+def do_file(ihelper, local_file, to_path, options={}):
+    """ Do metadata extraction, file upload, and metadata attachment for the given file,
+        locating it at the given 'to_path'.
+    """
     verbose = options.get("verbose", False)
-    metadata = fo.fits_metadata(file_path, options)
-    ihelper = ih.IrodsHelper(options=options)
-    ensure_astrolabe_root(ihelper, options)
-    ihelper.put_file(file_path)
-    ihelper.put_metaf(file_path, metadata)
+    ihelper.put_file(local_file, to_path)
+    metadata = fo.fits_metadata(local_file, options)
+    ihelper.put_metaf(metadata, to_path)
     return True
 
 
@@ -38,17 +39,7 @@ def do_tree(action, root_path, options):
     # action_cleanup(action, images_path, options)
 
 
-def filter_file_tree(root_dir):
-    """ Generator to yield all FITS files in the file tree under the given root directory. """
-    fits_pat = "*.fits"                     # pattern for identifying FITS files
-    gzfits_pat = "*.fits.gz"                # pattern for identifying gzipped FITS files
-    for root, dirs, files in os.walk(root_dir):
-        for fyl in files:
-            if (fnmatch.fnmatch(fyl, fits_pat) or fnmatch.fnmatch(fyl, gzfits_pat)):
-                file_path = os.path.join(root, fyl)
-                yield file_path
-
-def ensure_astrolabe_root(ihelper, options):
+def ensure_astrolabe_root(ihelper):
     """ Ensure that the Astrolabe root directory exists and set it as the user's root directory. """
     ihelper.set_root()                      # reset root to user iRod root dir
     ihelper.cd_root()                       # set cwd to user iRod root dir
