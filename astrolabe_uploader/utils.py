18662eece84166959d11c3ee137723196142460b
Tom Hicks <hickst@tohono.com>
2018-07-31 17:59:02 -0700

    Refactor out FITS file patterns and test method.

--- astrolabe_uploader/utils.py
+++ astrolabe_uploader/utils.py
@@ -1,10 +1,17 @@
 #
 # Module to provide general utility functions for Astrolabe code.
 #   Written by: Tom Hicks. 7/26/2018.
-#   Last Modified: Initial creation by aggregation of existing pieces.
+#   Last Modified: Refactor out FITS file patterns and test method.
 #
 import os
 import fnmatch
+
+_FITS_PAT = "*.fits"                        # pattern for identifying FITS files
+_GZFITS_PAT = "*.fits.gz"                   # pattern for identifying gzipped FITS files
+
+def is_fits_file(fyl):
+    """ Return True if the given file is FITS file, else False. """
+    return (fnmatch.fnmatch(fyl, _FITS_PAT) or fnmatch.fnmatch(fyl, _GZFITS_PAT))
 
 def filter_file_tree(root_dir):
     """ Generator to yield all FITS files in the file tree under the given root directory. """
@@ -12,6 +19,6 @@ def filter_file_tree(root_dir):
     gzfits_pat = "*.fits.gz"                # pattern for identifying gzipped FITS files
     for root, dirs, files in os.walk(root_dir):
         for fyl in files:
-            if (fnmatch.fnmatch(fyl, fits_pat) or fnmatch.fnmatch(fyl, gzfits_pat)):
+            if (is_files_file(fyl)):
                 file_path = os.path.join(root, fyl)
                 yield file_path
