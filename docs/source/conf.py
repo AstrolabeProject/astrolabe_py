85de88cbc2638f3e8e6a85737453a9d184709da3
Tom Hicks <hickst@tohono.com>
2018-08-06 15:00:04 -0700

    Customize Sphinx config file for this project.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'astrolabe-py'
-copyright = '2018, Tom Hicks'
+copyright = '2018, by the American Astronomical Society and the University of Arizona'
 author = 'Tom Hicks'
 
 # The short X.Y version
-version = ''
+version = '0.11'
 # The full version, including alpha/beta/rc tags
-release = '0.0.11'
+release = '0.0.11alpha'
 
 
 # -- General configuration ---------------------------------------------------
@@ -76,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
