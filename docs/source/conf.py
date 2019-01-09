dc6f4baf88b3dd4fafe94d5168f06d719526ebcf
Tom Hicks <hickst@tohono.com>
2019-01-09 16:19:44 -0700

    Update for Python requests lib vulnerability. Increment project version to 1.0.1.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, by the American Astronomical Society and the University of Ar
 author = 'Tom Hicks'
 
 # The short X.Y version
-version = '1.0'
+version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
