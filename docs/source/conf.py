856e70feddd76e3b646439eaee92bf76016a56d6
Tom Hicks <hickst@tohono.com>
2019-02-07 11:14:01 -0700

    Update project version to 1.1.0.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Tom Hicks'
 # The short X.Y version
 version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
